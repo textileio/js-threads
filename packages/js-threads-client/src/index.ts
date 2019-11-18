ddd775935317be2175cafcf375edbd4fa29f6e67
Sander Pick <sanderpick@gmail.com>
2019-11-18 12:05:45 -0800

    Merge pull request #1 from textileio/sander/subscriptions
    subs: add subscription handlers

--- packages/js-threads-client/src/index.ts
+++ packages/js-threads-client/src/index.ts
@@ -3,11 +3,27 @@ import * as uuid from 'uuid'
 const WebSocket = require('isomorphic-ws')
 const pack = require('../package.json')
 
-interface Response {
-  id: string
-  status: 'ok' | 'error'
+interface Message {
+  id?: string
+  type?: string
+  status?: 'ok' | 'error'
   body?: string
   error?: string
+}
+
+export interface LogRecord {
+  id: string
+  log_id?: string
+  thread_id: string
+  record_node?: string
+  event_node?: string
+  header_node?: string
+  body_node?: string
+}
+
+interface Subscription {
+  threads: string[]
+  handler: (rec: LogRecord) => void
 }
 
 export class Client {
@@ -17,10 +33,11 @@ export class Client {
   }
 
   private socket: WebSocket | undefined
-  private requests: Record<string, (res: Response) => void> = {}
+  private requests: Record<string, (res: Message) => void> = {}
+  private subscriptions: Record<string, Subscription> = {}
 
   public async connect(url: string) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       this.socket = new WebSocket(url) as WebSocket
 
       this.socket.onopen = () => {
@@ -32,6 +49,7 @@ export class Client {
       }
       this.socket.onerror = (e) => {
         console.error(e)
+        reject(e)
       }
       this.socket.onmessage = (e) => {
         this.handleEvent(e)
@@ -63,8 +81,15 @@ export class Client {
     return this.send('getRecord', [threadID, recordID])
   }
 
-  public async subscribe(...threads: string[]) {
-    return this.send('subscribe', threads)
+  public async subscribe(threads: string[], handler: (rec: LogRecord) => void) {
+    return this.send('subscribe', threads).then(() => {
+      const token = uuid.v4()
+      this.subscriptions[token] = {
+        threads,
+        handler
+      }
+      return token
+    })
   }
 
   protected async send(method: string, args: string[]) {
@@ -75,13 +100,11 @@ export class Client {
       }
 
       const id = uuid.v4()
-
-      this.requests[id] = (res: Response) => {
-        console.debug('Response: ', res)
-        if (res.error !== undefined) {
+      this.requests[id] = (res: Message) => {
+        if (res.error) {
           reject(new Error(res.error))
-        } else if (res.body !== undefined) {
-          resolve(JSON.parse(res.body))
+        } else if (res.body) {
+          resolve(JSON.parse(res.body) as LogRecord)
         } else {
           resolve()
         }
@@ -96,10 +119,32 @@ export class Client {
   }
 
   private handleEvent(event: MessageEvent) {
-    const res = JSON.parse(event.data)
-    if (this.requests[res.id] !== undefined) {
-      this.requests[res.id](res)
-      delete this.requests[res.id]
+    const res = JSON.parse(event.data) as Message
+
+    switch (res.type) {
+      case 'newRecord':
+        if (res.body === undefined) {
+          console.error('missing message body')
+          return
+        }
+        const body = JSON.parse(res.body) as LogRecord
+        Object.keys(this.subscriptions).forEach((token) => {
+          const sub = this.subscriptions[token]
+          sub.threads.forEach((id) => {
+            if (id === body.thread_id) {
+              sub.handler(body)
+            }
+          })
+        })
+        break
+      case 'response':
+        if (res.id && this.requests[res.id]) {
+          this.requests[res.id](res)
+          delete this.requests[res.id]
+        }
+        break
+      default:
+        console.error(new Error('unknown message type: ' + res.type))
     }
   }
 }

d27d74175136b204bee20be0290e6d6439e75cf9
Carson Farmer <carson.farmer@gmail.com>
2020-03-02 22:01:41 -0800

    chore: just so i don't lose this
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- packages/store/src/dispatcher.spec.ts
+++ packages/store/src/dispatcher.spec.ts
@@ -1,4 +1,6 @@
 import { expect } from 'chai'
+import { MemoryDatastore, Key, Result } from 'interface-datastore'
+import { collect } from 'streaming-iterables'
 import { Dispatcher } from './dispatcher'
 
 interface TestEvent {
@@ -10,18 +12,17 @@ interface TestEvent {
 describe('Dispatcher', () => {
   it('should not require any arguments to initialize', async () => {
     const d = new Dispatcher<TestEvent>()
-    const event: TestEvent = {
+    const value: TestEvent = {
       time: Date.now(),
       entity: 'null',
       collection: 'null',
     }
-    await d.dispatch(event)
-    // @todo: Should we check something here?
+    await d.dispatch({ key: new Key('key'), value })
   })
 
   it('should add new (unique) reducers on registration', async () => {
     const d = new Dispatcher<TestEvent>()
-    const reducer = { reduce: (..._event: TestEvent[]) => Promise.resolve(undefined) }
+    const reducer = { reduce: (..._event: Result<TestEvent>[]) => Promise.resolve(undefined) }
     await d.register(reducer)
     expect(d.reducers).to.have.length(1)
     await d.register(reducer)
@@ -30,54 +31,68 @@ describe('Dispatcher', () => {
 
   it('should only dispatch one (set of) events at a time', async () => {
     const d = new Dispatcher<TestEvent>()
-    const slowReducer = (..._event: TestEvent[]) => new Promise<void>(r => setTimeout(r, 2000))
+    const slowReducer = (..._event: Result<TestEvent>[]) => new Promise<void>(r => setTimeout(r, 2000))
     d.register({ reduce: slowReducer })
-    const event: TestEvent = {
+    const value: TestEvent = {
       time: Date.now(),
       entity: 'null',
       collection: 'null',
     }
     const t1 = Date.now()
     // Don't await...
-    d.dispatch(event)
-    await d.dispatch(event)
+    d.dispatch({ key: new Key('one'), value })
+    await d.dispatch({ key: new Key('two'), value })
     const t2 = Date.now()
     expect(t2 - t1 + 100).to.be.greaterThan(4000) // Adjust up to catch approx. timings
   }).timeout(5000)
 
-  it('should throw on first error', async () => {
-    const d = new Dispatcher<TestEvent>()
-    const event: TestEvent = {
+  it('should persist events to the internal store when present', async () => {
+    const d = new Dispatcher<TestEvent>(new MemoryDatastore())
+    const value: TestEvent = {
       time: Date.now(),
       entity: 'null',
       collection: 'null',
     }
-    await d.dispatch(event)
+    await d.dispatch({ key: new Key('one'), value })
+    await d.dispatch({ key: new Key('two'), value })
+    expect(d.store).to.not.be.undefined
+    expect(await collect(d.store?.query({}) || [])).to.have.lengthOf(2)
+  }).timeout(5000)
+
+  it('should throw on first error', async () => {
+    const d = new Dispatcher<TestEvent>()
+    const value: TestEvent = {
+      time: Date.now(),
+      entity: 'null',
+      collection: 'null',
+    }
+    await d.dispatch()
     // Error reducer
+    const error = new Error('error')
     const reducer = {
-      reduce: (..._event: TestEvent[]) => Promise.reject(new Error('error')),
+      reduce: (..._event: Result<TestEvent>[]) => Promise.reject(new Error('error')),
     }
     await d.register(reducer)
     try {
-      await d.dispatch(event)
+      await d.dispatch({ key: new Key('one'), value })
       throw new Error('should have thrown error on dispatch call')
     } catch (err) {
-      expect(err.toString()).to.equal('Error: error')
+      expect(err.toString()).to.equal(error.toString())
     }
   })
 
   it('should not be able to register a new reducer while dispatching', async () => {
     const d = new Dispatcher<TestEvent>()
-    const slowReducer = (..._event: TestEvent[]) => new Promise<void>(r => setTimeout(r, 2000))
+    const slowReducer = (..._event: Result<TestEvent>[]) => new Promise<void>(r => setTimeout(r, 2000))
     await d.register({ reduce: slowReducer })
-    const event: TestEvent = {
+    const value: TestEvent = {
       time: Date.now(),
       entity: 'null',
       collection: 'null',
     }
     const t1 = Date.now()
     // Don't await...
-    d.dispatch(event)
+    d.dispatch({ key: new Key('one'), value })
     await d.register({ reduce: slowReducer })
     const t2 = Date.now()
     expect(t2 - t1).to.be.greaterThan(2000 - 10)
--- packages/store/src/dispatcher.ts
+++ packages/store/src/dispatcher.ts
@@ -1,13 +1,18 @@
-import { RWLock } from 'async-rwlock'
 import log from 'loglevel'
+import { ulid } from 'ulid'
+import { decode } from 'cbor-sync'
+import { Datastore, Key, Result } from 'interface-datastore'
+import { map } from 'streaming-iterables'
+import { encode } from 'cbor-sync'
+import { RWLock } from 'async-rwlock'
 
 const logger = log.getLogger('store:dispatcher')
 
 /**
  * Reducer applies an event to an existing state.
  */
-export interface Reducer<T extends object = any> {
-  reduce(...events: T[]): Promise<void>
+export interface Reducer<T = any> {
+  reduce(...events: Result<T>[]): Promise<void>
 }
 
 /**
@@ -16,35 +21,55 @@ export interface Reducer<T extends object = any> {
  * Every event is dispatched to every registered reducer. Dispatcher is based on the singleton dispatcher utilized
  * in the "Flux" pattern (see https://github.com/facebook/flux).
  */
-export class Dispatcher<T extends object = any> {
-  private lock: RWLock = new RWLock()
-  public reducers: Set<Reducer<T>> = new Set()
+export class Dispatcher extends RWLock {
+  public reducers: Set<Reducer<any>> = new Set()
+
+  /**
+   * Dispatcher creates a new dispatcher.
+   * @param store The optional event 'log' to persist events. If undefined this is treated as a stateless dispatcher.
+   */
+  constructor(public store?: Datastore<Buffer>) {
+    super()
+  }
 
   /**
    * Register takes a reducer to be invoked with each dispatched event.
    * @param reducer A reducer for processing dispatched events.
    */
-  async register(reducer: Reducer<T>) {
-    await this.lock.writeLock()
+  async register<T>(reducer: Reducer<T>) {
+    await this.writeLock()
     this.reducers.add(reducer)
-    this.lock.unlock()
+    this.unlock()
     logger.debug(`registered reducers: ${this.reducers.size}`)
+  }
+
+  replay(prefix?: string) {
+    const mapper = ({ key, value }: Result<Buffer>) => {
+      const result: Result<any> = { key: new Key(key.type()), value: decode(value) }
+      return result
+    }
+    return map(mapper, this.store?.query({ prefix }) || [])
   }
 
   /**
    * Dispatch dispatches a payload to all registered reducers.
    * @param events The (variadic list of) events to dispatch.
    */
-  async dispatch(...events: T[]) {
-    await this.lock.writeLock()
+  async dispatch<T>(...events: Result<T>[]) {
+    await this.writeLock()
     try {
-      logger.debug(`dispatching reducers: ${this.reducers.size}`)
-      await Promise.all([...this.reducers].map(reducer => reducer.reduce(...events)))
-    } catch (err) {
-      logger.error(err.toString())
-      throw err
+      if (this.store) {
+        logger.debug('persisting events')
+        const batch = this.store.batch()
+        for (const { key, value } of events) {
+          batch.put(key.instance(ulid()), encode(value))
+        }
+        await batch.commit()
+      }
+      logger.debug('dispatching')
+      await Promise.all([...this.reducers].map((reducer: Reducer<T>) => reducer.reduce(...events)))
     } finally {
-      this.lock.unlock()
+      this.unlock()
     }
   }
 }

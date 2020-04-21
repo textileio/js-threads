f546113a18901ba67fe89621613dbc03c7cc0749
Carson Farmer <carson.farmer@gmail.com>
2020-04-21 09:44:50 -0700

    chore: update randombytes version
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- packages/id/src/randombytes.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '@consento/sync-randombytes' {
-  import { Buffer } from 'buffer'
-
-  // eslint-disable-next-line import/no-default-export
-  export default function <T extends Uint8Array | Buffer>(input: T): T;
-}

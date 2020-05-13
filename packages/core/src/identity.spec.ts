75f95f8911e74175d06962045fcf73986fd9d076
Carson Farmer <carson.farmer@gmail.com>
2020-05-12 17:31:52 -0700

    fix: correct test name etc
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- packages/core/src/identity.spec.ts
+++ packages/core/src/identity.spec.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai'
 import { Libp2pCryptoIdentity } from './identity'
 
-describe('ThreadKey', () => {
-  it('should be able to create a random ThreadKey', async () => {
+describe('Identity', () => {
+  it('should be able to serialize and recover identities', async () => {
     const id = await Libp2pCryptoIdentity.fromRandom()
     const str = id.toString()
     const back = await Libp2pCryptoIdentity.fromString(str)

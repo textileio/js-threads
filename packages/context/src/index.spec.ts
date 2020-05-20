4fb96e9799c59b4f44b0a0f0b06db278c98f0b55
Carson Farmer <carson.farmer@gmail.com>
2020-05-20 14:29:37 -0700

    feat: ensure also passes on future dates
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- packages/context/src/index.spec.ts
+++ packages/context/src/index.spec.ts
@@ -3,8 +3,8 @@ import { Context, expirationError } from './index'
 
 describe('Context', () => {
   it('should throw an exception when working with an expired msg', async () => {
+    const context = new Context()
     try {
-      const context = new Context()
       context.withAPISig({
         sig: 'fake',
         // Create msg date in the past
@@ -15,5 +15,12 @@ describe('Context', () => {
     } catch (err) {
       expect(err).to.equal(expirationError)
     }
+    const msg = new Date(Date.now() + 1000 * 60).toUTCString()
+    context.withAPISig({
+      sig: 'fake',
+      msg,
+    })
+    const json = context.toJSON()
+    expect(json).to.haveOwnProperty('x-textile-api-sig-msg', msg)
   })
 })

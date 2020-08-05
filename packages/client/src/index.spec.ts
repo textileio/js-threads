d659e757798be6da0e64e8ef4837896fe521086b
Carson Farmer <carson.farmer@gmail.com>
2020-08-05 12:18:17 -0700

    fix: type fix
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- packages/client/src/index.spec.ts
+++ packages/client/src/index.spec.ts
@@ -401,11 +401,11 @@ describe("Client", function () {
     }).timeout(5000) // Make sure our test doesn't timeout
 
     it("should handle deletes.", (done) => {
-      const callback = (reply: Update<Person>, err?: Error) => {
+      const callback = (reply?: Update<Person>, err?: Error) => {
         if (err) {
           throw err
         }
-        expect(reply.instance).to.be.undefined
+        expect(reply?.instance).to.be.undefined
         if (listener.close) {
           listener.close()
         }
